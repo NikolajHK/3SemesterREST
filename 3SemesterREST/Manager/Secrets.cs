e22d8472308143fc611b0cbba2db67ac3a35b986
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2021-05-03 14:36:01 +0200

    Tilføjet connection string til database

---
+++ 3SemesterREST/Manager/Secrets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3SemesterREST.Manager
+{
+    public class Secrets
+    {
+        public const string ConnectionString =
+
+            "Server=tcp:AndenSemester.database.windows.net,1433; Database=AndenSemester; User Id=SørenYasin; Password=NielsenYasin2E";
+
+    }
+}
